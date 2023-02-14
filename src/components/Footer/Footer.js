8ec32cd2bb8630dc77eb3cd0b8ef66038618497d
mavandop <mavandop@indeed.com>
2023-02-13 21:30:26 -0800

    footer update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="page-footer font-small  pt-4">
-      <div className="footer-copyright p-2">
+      <div className="footer-copyright p-4 text-center">
         © 2023 Copyright: Westwind Property Maintenance
       </div>
     </footer>
