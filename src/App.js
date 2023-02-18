721fb4aeaf3df3e24174041a475d7f88a38b44b1
mavandop <mavandop@indeed.com>
2023-02-17 21:51:01 -0800

    cleanup files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { HomePage } from "./pages/Home";
-import { Contact } from "./pages/Contact";
-import { Testimonials } from "./pages/Testimonials";
 import MainNavbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import { ThemeProvider } from "react-bootstrap";
@@ -18,8 +16,6 @@ const App = () => (
       <MainNavbar />
       <Switch>
         <Route path="/" exact component={HomePage} />
-        <Route path="/contact" exact component={Contact} />
-        <Route path="/testimonials" exact component={Testimonials} />
       </Switch>
       <Footer />
     </BrowserRouter>
